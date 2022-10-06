8aa194b3b0119788bf656efc3d3de0dbaabce71b
skarthik <soma.nitt@gmail.com>
2022-10-06 16:35:18 -0400

    changes v2

--- modcon/solution/src/encoder_pose/src/PID_controller_homework.py
+++ modcon/solution/src/encoder_pose/src/PID_controller_homework.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[59]:
+# In[124]:
 
 
 import numpy as np
@@ -47,9 +47,9 @@ def PIDController(
     e_der_y = (e_y - prev_e_y)/delta_t
 
     # controller coefficients
-    Kp = 0.10
-    Ki = 0.01
-    Kd = 10
+    Kp = 0.1
+    Ki = 0.000000
+    Kd = 5
     
     # PID controller for omega
     omega = Kp*e_y + Ki*e_int_y + Kd*e_der_y
